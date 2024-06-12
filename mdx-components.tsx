bf2bcb05dd9a6cfce4a8e3fe438ade1589bf2047
AlexW29ChemE <103972325+AlexW29ChemE@users.noreply.github.com>
2024-06-12 20:27:50 +1000

    Initial Project commit

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,10 +1,8 @@
-import type { MDXComponents } from "mdx/types";
-
-// This file is required to use MDX in `app` directory.
-export function useMDXComponents(components: MDXComponents): MDXComponents {
-  return {
-    // Allows customizing built-in components, e.g. to add styling.
-    // h1: ({ children }) => <h1 style={{ fontSize: "100px" }}>{children}</h1>,
-    ...components,
-  };
+import { MDXRemote } from 'next-mdx-remote/rsc'
+ 
+export default async function RemoteMdxPage() {
+  // MDX text - can be from a local file, database, CMS, fetch, anywhere...
+  const res = await fetch('')
+  const markdown = await res.text()
+  return <MDXRemote source={markdown} />
 }
